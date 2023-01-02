401d921edfef1664f51e38505de61c761e3562ae
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2023-01-02 16:08:31 -0500

    fleshing out structure of game loop

--- game.js
+++ game.js
@@ -1,17 +1,119 @@
-"use strict"
+
 
 //constants
 var BLOCK_SIZE = 30;
-var COLS = 10;
-var ROWS = 20;
+var COLS = 12;
+var ROWS = 22;
+var canvas;
+var ctx;
+var playStatus = false;
+var gameStart = false;
+var playBtn = document.getElementById("play-btn")
+var pauseBtn = document.getElementById("pause-btn")
+var resetBtn = document.getElementById("reset-btn")
+var pieces = [];
+
 
 //canvas and context, defines default fill color to red and rows/cols, also scales ctx
-var canvas = document.getElementById("gameCanvas");
-canvas.height = BLOCK_SIZE * ROWS;
-canvas.width = BLOCK_SIZE * COLS;
 
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000"
+
+window.onload = function() {
+    canvas = document.getElementById("gameCanvas");
+    canvas.height = BLOCK_SIZE * ROWS;
+    canvas.width = BLOCK_SIZE * COLS;
+
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000"
+    ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+
+
+    playBtn.addEventListener('click', gameLoop, false);
+    pauseBtn.addEventListener('click', pauseFunc, false);
+    resetBtn.addEventListener('click', resetBoard, false);
+    
+
+}
+
+// game loop logic
+
+gameLoop = function() {
+    playStatus = true;
+    setInterval(update, 1000/10)
+}
+
+pauseFunc = function() {
+    playStatus = false;
+}
+
+resetBoard = function() {
+    playStatus = false;
+    reset()
+}
+
+function update() {
+    let mainPiece;
+
+    if (playStatus) { // play game
+
+        if (generateNewPieceCond()) {
+            mainPiece = new Piece();
+            renderPiece(mainPiece);
+        }
+
+
+
+    } else { // freeze game
+
+    }
+
+
+    
+}
+
+function reset() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    gameStart = false;
+}
+
+
+// piece logic
+function renderPiece(piece) {
+    
+    for (let i = 0; i<piece.shape.length; i++) {
+        for (let j = 0; j<piece.shape[i].length; j++) {
+            if (piece.shape[i][j] > 0) {
+                ctx.fillRect(piece.x+j, piece.y + i, 1, 1)
+            }
+        }
+    }
+
+    return piece;
+}
+
+function generateNewPieceCond() { // determines whether new piece should be generated or not
+    // new piece should be generated when:
+    // game starts
+    // current piece is "set" on the board
+    
+    if (!gameStart) {
+        gameStart = true;
+        return true;
+    } else if ([]) { // conditional for when current piece is set on the board
+
+    }
+    
+}
+
+
+
+
+
+// play/pause logic
+
+
+// end game logic
+
 
 
 
@@ -35,18 +137,21 @@ class Board {
           );
     }
 
-    draw() { //renders pieces on board
+    draw() { //renders pieces on board, called everytime a new piece needs to be rendered
 
     }
 
 }
 
-//piece class
+// classes
 class Piece { // responsible for supplying color, shape, and location of piece
     constructor() {
-        this.x = 5;
+        this.x = 4;
         this.y = 0;
+        this.velocity = -1;
         this.color = this.chooseRandomPieceColor();
+        this.shape = this.determineShape()
+        pieces.push(this);
     }
 
     chooseRandomPieceColor() {
@@ -55,6 +160,43 @@ class Piece { // responsible for supplying color, shape, and location of piece
         let color = colorArray[num];
 
         return color;
+    }
+
+    determineShape() {
+        const SHAPES = [
+            [[0, 0, 0, 0], 
+            [1, 1, 1, 1], 
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0]],
+    
+            [[2, 0, 0], 
+            [2, 2, 2], 
+            [0, 0, 0]],
+    
+            [[0, 0, 3], // 0,0 -> 2,0 ; 0,1 -> 1,0 ; 0,2 -> 0,0
+             [3, 3, 3], // 1,0 -> 2,1 ; 1,1 -> 1,1 ; 1,2 -> 0,1 
+             [0, 0, 0]],// 2,0 -> 2,2 ; 2,1 -> 1,2 ; 2,2 -> 0,2
+    
+            [[4, 4], 
+            [4, 4]],
+    
+            [[0, 5, 5], 
+            [5, 5, 0], 
+            [0, 0, 0]],
+    
+            [[0, 6, 0], 
+            [6, 6, 6], 
+            [0, 0, 0]],
+    
+            [[7, 7, 0], 
+            [0, 7, 7], 
+            [0, 0, 0]]
+        ];
+
+        let index = Math.floor(Math.random() * SHAPES.length);
+        let pieceShape = SHAPES[index];
+
+        return pieceShape;
     }
 
 
@@ -67,15 +209,6 @@ class Piece { // responsible for supplying color, shape, and location of piece
 
 //board
 let board = new Board();
-
-function playGame() {
-    console.table(board.grid);
-    ctx.fillRect(0, 0, 20, 20);
-}
-
-function reset() {
-    board.resetBoard()
-}
 
 
 
